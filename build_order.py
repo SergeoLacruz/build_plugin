e727bb266f690fc03e99277498d3e151f51f7046
Michael <michael@buchmann.ruhr>
2024-11-11 20:15:04 +0100

    Cosmetic code cleaning

--- build_order.py
+++ build_order.py
@@ -2,13 +2,13 @@ from django.urls import re_path
 from django.http import HttpResponse
 
 from build.views import BuildDetail
-from build.models import Build
 from plugin import InvenTreePlugin
 from plugin.mixins import PanelMixin, SettingsMixin, UrlsMixin, ReportMixin
 from company.models import Company, Contact
 from users.models import check_user_role
 
 import json
+
 
 class BuildOrderPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin, ReportMixin):
 
@@ -50,7 +50,7 @@ class BuildOrderPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin, Rep
         },
     }
 
-# Create some help  
+# Create some help
     def get_settings_content(self, request):
         return """
         <p>Setup:</p>
@@ -65,89 +65,87 @@ class BuildOrderPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin, Rep
 # Create the panel that will display on the BuildOrder view,
     def get_custom_panels(self, view, request):
         panels = []
-        parameters = ['PARAMETER_WIDTH','PARAMETER_LENGTH','PARAMETER_LAYNO','PARAMETER_DUAL']
+        parameters = ['PARAMETER_WIDTH', 'PARAMETER_LENGTH', 'PARAMETER_LAYNO', 'PARAMETER_DUAL']
         if isinstance(view, BuildDetail):
-            self.build=view.get_object()
-            self.companies=Company.objects.filter(is_supplier=True)
-            self.contacts=Contact.objects.filter()
+            self.build = view.get_object()
+            self.companies = Company.objects.filter(is_supplier=True)
+            self.contacts = Contact.objects.filter()
 
             # Grab PCB data from the PCB parameters
             self.build_data = {}
-            related_pcb=list(self.build.part.get_related_parts())[0]
+            related_pcb = list(self.build.part.get_related_parts())[0]
             self.build_data['pcb_name'] = related_pcb.IPN
             for parameter in related_pcb.parameters.all():
                 for par in parameters:
-                    try: 
+                    try:
                         if parameter.template.pk == int(self.get_setting(par)):
-                            self.build_data[par]=parameter.data
+                            self.build_data[par] = parameter.data
                     except Exception:
-                        self.build_data[par]='Parameter pk not defined'
+                        self.build_data[par] = 'Parameter pk not defined'
 
             # Find our company and the contacts für the EMS partner
             try:
-                customer_pk=int(self.get_setting('MY_PK'))
-            except:
-                raise ValueError('MY_PK in properly set. Please check settings')       
-            self.customer_company=Company.objects.get(pk=customer_pk)
-            self.all_customer_contacts=Contact.objects.filter(company=customer_pk)
+                customer_pk = int(self.get_setting('MY_PK'))
+            except Exception:
+                raise ValueError('MY_PK in properly set. Please check settings')
+            self.customer_company = Company.objects.get(pk=customer_pk)
+            self.all_customer_contacts = Contact.objects.filter(company=customer_pk)
 
             # Select the attachments
             for p in self.build.attachments.all():
-                print('Name:',p.comment)
-                print('Name:',p.attachment)
+                print('Name:', p.comment)
+                print('Name:', p.attachment)
 
             # Calculate the total number of components on the board
-            self.build_data['total_components']=0
+            self.build_data['total_components'] = 0
             for p in self.build.part.bom_items.all():
                 self.build_data['total_components'] = self.build_data['total_components'] + p.quantity
 
             # Grab metadata if exist and put it into the build_data dict
             try:
                 self.build_data['ems_company'] = Company.objects.get(pk=self.build.metadata['ems_company_pk'])
-            except:
+            except Exception:
                 print('error ems_company_pk')
             try:
-                self.build_data['ems_contact']=Contact.objects.get(pk=self.build.metadata['ems_contact_pk'])
-            except:
+                self.build_data['ems_contact'] = Contact.objects.get(pk=self.build.metadata['ems_contact_pk'])
+            except Exception:
                 print('error ems_contact_pk')
             try:
-                self.build_data['customer_contact']=Contact.objects.get(pk=self.build.metadata['customer_contact'])
-            except:
+                self.build_data['customer_contact'] = Contact.objects.get(pk=self.build.metadata['customer_contact'])
+            except Exception:
                 print('error customer_contact')
             try:
-                self.build_data['material_provisioning']=self.build.metadata['material_provisioning']
-            except:
+                self.build_data['material_provisioning'] = self.build.metadata['material_provisioning']
+            except Exception:
                 print('error material_provisioning')
             try:
-                self.build_data['sample_approval']=self.build.metadata['sample_approval']
-            except:
+                self.build_data['sample_approval'] = self.build.metadata['sample_approval']
+            except Exception:
                 print('error sample_approval')
 
-            has_permission=(check_user_role(view.request.user, 'build_order','change') or 
-                           check_user_role(view.request.user, 'build_order','delete') or
-                           check_user_role(view.request.user, 'build_order','add'))
+            has_permission = (check_user_role(view.request.user, 'build_order', 'change') or
+                              check_user_role(view.request.user, 'build_order', 'delete') or
+                              check_user_role(view.request.user, 'build_order', 'add'))
             if has_permission:
                 panels.append({
                     'title': 'Manufacturig Info',
                     'icon': 'fa-industry',
-                    'content_template': 'build_panel/build.html', 
+                    'content_template': 'build_panel/build.html',
                 })
         return panels
 
     def setup_urls(self):
-        return [
-                re_path(r'bocompanyselect(?:\.(?P<format>json))?$', self.process_data, name='transfer_build_data'),
-               ]
+        return [re_path(r'bocompanyselect(?:\.(?P<format>json))?$', self.process_data, name='transfer_build_data')]
 
-#------------------------- Helper functions ------------------------------------
-    def process_data(self,request):
+# ------------------------- Helper functions ------------------------------------
+    def process_data(self, request):
 
-        data=json.loads(request.body)
+        data = json.loads(request.body)
         if self.build.metadata is None:
-            self.build.metadata={}
+            self.build.metadata = {}
         for key in data:
             print(key, data[key])
-            self.build.metadata[key]=data[key]
+            self.build.metadata[key] = data[key]
         self.build.save()
         self.build_data['ems_company'] = Company.objects.get(pk=self.build.metadata['ems_company_pk'])
         try:
@@ -155,20 +153,16 @@ class BuildOrderPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin, Rep
         except Exception:
             pass
         self.build_data['customer_contact'] = Contact.objects.get(pk=self.build.metadata['customer_contact'])
-        self.build_data['material_provisioning']=self.build.metadata['material_provisioning']
-        self.build_data['sample_approval']=self.build.metadata['sample_approval']
-        return HttpResponse(f'OK')
+        self.build_data['material_provisioning'] = self.build.metadata['material_provisioning']
+        self.build_data['sample_approval'] = self.build.metadata['sample_approval']
+        return HttpResponse('OK')
 
-#-------------------- Add context data for report generation -------------------
+# -------------------- Add context data for report generation -------------------
     def add_report_context(self, report_instance, model_instance, request, context):
 
-#        print('report_instance', report_instance)
-#        print('request', request)
-#        print('context', context)
-#        print('model_instance', model_instance)
+        # print('report_instance', report_instance)
+        # print('request', request)
+        # print('context', context)
+        # print('model_instance', model_instance)
 
         context['build_plugin'] = self
-
-
-
-
