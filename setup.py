451085f74c0121475eca9c27ebbd2cedac25c1b9
Michael <michael@buchmann.ruhr>
2024-11-16 20:38:28 +0100

    add setup

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ from inventree_build_data.version import PLUGIN_VERSION
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-
 setuptools.setup(
     name="inventree-build-data",
     version=PLUGIN_VERSION,
     author="Michael Buchmann",
     author_email="michael@buchmann.ruhr",
-    description="Assign additional data to a build for external manufacturing^",
+    description="Assign additional data to a build for external manufacturing",
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords="inventree build inventory ems manufacturing",
